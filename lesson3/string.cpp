e49f9b5a4205ac2267331c025ac468f00fd989f7
Misha <113121252+MykhailoTymoshenko08@users.noreply.github.com>
2026-01-16 01:01:57 +0200

    Add files via upload

---
+++ lesson3/string.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+#include <cstring>
+//size_t strlen (const char* str)    without \0
+//char *strcpy (char *st1, char *st2)
+//char strncpy (char *s, char *st, char *st, n)
+
+//char *strcat (char *st1, const *st2)
+//char *strncat (char *st1, const char *st2, int n)
+
+//int strcmp (char *st1, char *st2)
+//int strncmp (char *st1, char *st2, n)
+//<0 if st1<st2
+//stricmp // if W=w
+
+//strlwr   W => w
+//strupr   w => W
+//strrew    hello => olleh
+
+//sizeof()
+//strtok
+
+//int atoi
+//long atol
+//float atof
+
+
+int main(){
+    // //len
+    // char st1[] = "I like C++!";
+    // cout << strlen(st1);
+    // //copy
+    // char str1 [50];
+    // strcpy(str1, "Hello, World");   // str1 = str2 !!!!! Cause str = & 
+    // //copy chars
+    // char str2[] = "Good";
+    // char str3[] = "Bred";
+    // strncpy(str1, str2, 1);
+    // //concatenation 
+    // char str4[100];
+    // strcat(str4, "I like");
+    // strcat(str4, "C++");
+    // //or
+    // char str5[100] = "Hi there";
+    // char str6[100] = " booy sigma";
+    // strncat(str5, str6, 4);
+    // //string comparison
+    // char str7[] = "Hi";
+    // char str8[] = "There";
+    // strcmp(str7, str8); //<0
+}
