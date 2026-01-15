a0a1f7aed5e5dc176fafe1f385dd9ece06733c68
Misha <113121252+MykhailoTymoshenko08@users.noreply.github.com>
2026-01-16 01:35:30 +0200

    Add files via upload

---
+++ lesson4/string.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main(){
+    string str1;
+    string str2 = "This is string";
+    string str3 ("The other string"); //constructor
+
+    string str4;
+    getline(cin, str4, '\n');
+
+    //size();
+    //lenght();
+    //empty();   if string is empty
+    //capacity();
+    //max_size();
+
+    //string a = "Hello";
+    //a.append(" World");
+    //a.insert(4, "Hello");
+    //a.erase(4, 7);
+    //a.replace(4, 2, "was")
+    //a.clear()
+
+    //a.find("word");    // only first face
+    //a.rfind("word");   // only last face
+    //a.find_first_of("");
+    //a.find_last_of("");
+    //a.find_first_not_of("");
+
+    //a.substr(3,5)
+    //a.compare(b)
+
+    //a.front();
+    //a.back();
+    //stoi();
+
+    //c_str
+    //string cpp_str(c_str);
+    //cpp_str = c_str;
+    //c_str = cpp_str.c_str();
+    //c_str = cpp_str.data(); 
+
+    //string mas [i]; -- matrix
+    //string *pst = new string
+}
